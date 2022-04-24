100e503bc502eef6da324f445f8e657680001d8d
Sakshham Bhagat <sakshhamtg@gmail.com>
2022-04-24 13:23:39 +0530

    Co-authored-by: Kitty <kitty788v>

--- main.py
+++ main.py
@@ -1,1 +1,62 @@
-from matplotlib import pyplot as plot
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import mysql.connector
+from mysql.connector import MySQLConnection
+
+# sqlcon:MySQLConnection = mysql.connector.connect(user="root", password="tobeadded")
+# cursor = sqlcon.cursor()
+
+start = True
+
+print("""
+───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+───█▒▒░░░░░░░░░▒▒█───
+────█░░█░░░░░█░░█────
+─▄▄──█░░░▀█▀░░░█──▄▄─
+█░░█─▀▄░░░░░░░▄▀─█░░█
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+""")
+
+while start:
+    dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+
+    dev_y = [38496, 42000, 46752, 49320, 53200,
+                56000, 62316, 64928, 67317, 68748, 73752]
+    print("What do you want to do?")
+    print("(1) View current graph")
+    print("(2) Edit values for graph")
+    print("(3) Exit")
+    input1 = int(input("Type your choice here (1/2/3): "))
+    if input1 == 1:
+        plt.plot(dev_x, dev_y)
+        plt.xlabel('Ages')
+        plt.ylabel('Median Salary (USD)')
+        plt.title('Median Salary (USD) by Age')
+        plt.show()
+    elif input1 == 2:
+        while True:
+            x = int(input("Input value to add on x-axis: "))
+            y = int(input("Input value to add on y-axis: "))
+            more = input("Do you want to add more items? (Y/N): ")
+            dev_x.clear()
+            dev_y.append(y)
+            if more.lower() == "n":
+                break
+            else: continue
+            
+    elif input1 == 3:
+        break
+    else: print("\n==========ERROR: Choose from 1/2/3==========\n")
+
+#                _                          
+#               | |                         
+#  __      _____| | ___ ___  _ __ ___   ___ 
+#  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \
+#   \ V  V /  __/ | (_| (_) | | | | | |  __/
+#    \_/\_/ \___|_|\___\___/|_| |_| |_|\___|
+                                          
+                                          
---
+++ test3.py
@@ -0,0 +1,135 @@
+# Importing Libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from numpy import random
+
+# Defining Colours for Printing Purposes
+red = (255,51,51)
+yellow = (255,255,102)
+green = (102,255,102)
+blue = (102,178,255)
+
+# Functions for printing a coloured text
+def print_colored(color, text):
+    print(f"\033[38;2;{color[0]};{color[1]};{color[2]}m{text}\033[38;2;255;255;255m")
+def input_colored(color, text):
+    input1 = input(f"\033[38;2;{color[0]};{color[1]};{color[2]}m{text}\033[38;2;255;255;255m")
+    return input1
+
+# Reading csv file and removing missing values
+df = pd.read_csv("daata.csv")
+df = df.dropna()
+
+
+print_colored(red,'''
+        ───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+        ───█▒▒░░░░░░░░░▒▒█───
+        ────█░░█░░░░░█░░█────          WELCOME TO SONG STAT GRAPH PROJECT
+        ─▄▄──█░░░▀█▀░░░█──▄▄─
+        █░░█─▀▄░░░░░░░▄▀─█░░█
+        █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█       - Made by: Sakshham   &   Vasundhra
+        █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█                   Bhagat         Sharma
+        █░░║║║╠─║─║─║║║║║╠─░░█
+        █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+
+''')
+
+def histogram():
+    reply5 = input(
+            "Which data would you like to view? \n (1) Danceability \n (2) Energy \n (3) Tempo \n (4)Time_Signature ")
+    if reply5 == "1":
+        df["Danceability"].plot(kind="hist", color="yellow")
+        plt.show()
+    elif reply5 == "2":
+        df["Energy"].plot(kind="hist", color="red")
+        plt.show()
+    elif reply5 == "3":
+        df["Tempo"].plot(kind="hist", color="green")
+        plt.show()
+    else:
+        df["Time_signature"].plot(kind="hist", color="purple")
+        plt.show()
+
+def axis_select(type: str):
+    random_color = str(random.choice(
+        ["red", "green", "blue", "yellow", "purple", "pink", "black", "brown"]))
+    random_marker = str(random.choice(["o", "d"]))
+    print("(1) Danceability \n (2) Energy \n (3) Tempo \n (4) Time_signature")
+    reply3 = input("Select X-axis - (1)/(2)/(3)/(4): ")
+    reply4 = input("Select Y-axis - (1)/(2)/(3)/(4): ")
+    if reply3 == "1" and reply4 == "2":
+        df.plot(kind=type, color=random_color,
+                marker=random_marker, x="Danceability", y="Energy")
+        plt.show()
+    elif reply3 == "1" and reply4 == "3":
+        df.plot(kind=type, color=random_color,
+                marker=random_marker, x="Danceability", y="Tempo")
+        plt.show()
+    elif reply3 == '1' and reply4 == '4':
+        df.plot(kind=type, color=random_color, marker=random_marker,
+                x="Danceability", y="Time_signature")
+        plt.show()
+    elif reply3 == '2' and reply4 == '1':
+        df.plot(kind=type, color=random_color,
+                marker=random_marker, x="Energy", y="Danceability")
+        plt.show()
+    elif reply3 == '2' and reply4 == '3':
+        df.plot(kind=type, color=random_color,
+                marker=random_marker, x="Energy", y="Tempo")
+        plt.show()
+    elif reply3 == '2' and reply4 == '4':
+        df.plot(kind=type, color=random_color, marker=random_marker,
+                x="Energy", y="Time_signature")
+        plt.show()
+    elif reply3 == '3' and reply4 == '1':
+        df.plot(kind=type, color=random_color,
+                marker=random_marker, x="Tempo", y="Danceability")
+        plt.show()
+    elif reply3 == '3' and reply4 == '2':
+        df.plot(kind=type, color=random_color,
+                marker=random_marker, x="Tempo", y="Energy")
+        plt.show()
+    elif reply3 == '3' and reply4 == '4':
+        df.plot(kind=type, color=random_color,
+                marker=random_marker, x="Tempo", y="Time_signature")
+        plt.show()
+    elif reply3 == '4' and reply4 == '1':
+        df.plot(kind=type, color=random_color, marker=random_marker,
+                x="Time_signature", y="Danceability")
+        plt.show()
+    elif reply3 == '4' and reply4 == '2':
+        df.plot(kind=type, color=random_color, marker=random_marker,
+                x="Time_signature", y="Energy")
+        plt.show()
+    elif reply3 == '4' and reply4 == '3':
+        df.plot(kind=type, color=random_color,
+                marker=random_marker, x="Time_signature", y="Tempo")
+        plt.show()
+    else:
+        print("Choose a valid option")
+        return
+
+
+print_colored(blue, "What do you want to do ? \n  (1) View graph \n  (2) Edit graph \n  (3) Exit")
+reply = input_colored(blue, "Select one - (1)/(2)/(3) : ")
+if reply == "1":
+    print(": VIEW GRAPHS : ")
+    print("Which type of graph do you want to view? \n (1) Line Graph \n (2) Bar Graph \n (3) Histogram \n (4) Pie Chart \n (5) Scatter")
+    reply2 = input("Select one - (1)/(2)/(3)/(4)/(5): ")
+    if reply2 == "1":
+        axis_select("line")
+    elif reply2 == "2":
+        axis_select("bar")
+    elif reply2 == "3":
+        histogram()
+    elif reply2 == "4":
+        print("pie here")
+    elif reply2 == "5":
+        axis_select("scatter")
+elif reply == "2":
+    print("edit here")
+elif reply == "3":
+    exit()
+else:
+    print("Choose a valid option")
